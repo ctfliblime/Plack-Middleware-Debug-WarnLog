6a9522663a8eee00e806a8f431555f2f90a7a550
Clay Fouts <cfouts@liblime.com>
2011-09-14 02:14:30 -0700

    Obviate need for WarnStash by overriding call()

--- lib/Plack/Middleware/Debug/WarnLog.pm
+++ lib/Plack/Middleware/Debug/WarnLog.pm
@@ -6,30 +6,40 @@ use parent qw(Plack::Middleware::Debug::Base);
 use strict;
 use warnings;
 
-use Plack::Middleware::WarnStash;
-
 our $VERSION = '0.001';
+
+use constant warn_key => 'plack.middleware.debug.warnlog';
+
+sub call {
+    my ($self, $env) = @_;
+
+    my $old_warn = $SIG{__WARN__} || sub { warn @_ };
+    local $SIG{__WARN__} = sub {
+        push @{$env->{&warn_key}}, @_;
+        $old_warn->(@_);
+    };
+
+    $self->SUPER::call($env);
+}
 
 sub run {
     my($self, $env, $panel) = @_;
 
-    my $warn_key = Plack::Middleware::WarnStash::warn_key();
-
     return sub{
         my $res = shift;
 
-        $env->{$warn_key} ||= [];
+        $env->{&warn_key} ||= [];
 
-        my $plural = (@{$env->{$warn_key}} != 1) ? 's' : '';
-        my $count = scalar @{$env->{$warn_key}} || 'No';
+        my $plural = (@{$env->{&warn_key}} != 1) ? 's' : '';
+        my $count = scalar @{$env->{&warn_key}} || 'No';
         my $subtitle = sprintf('%s warning%s', $count, $plural);
 
         $panel->nav_title('Warnings');
         $panel->nav_subtitle($subtitle);
-        $panel->content($self->render_lines($env->{$warn_key}));
+        $panel->content($self->render_lines($env->{&warn_key}));
 
         # Just so it doesn't also show up in the 'Environment' panel
-        delete $env->{$warn_key};
+        delete $env->{&warn_key};
     };
 }
 
@@ -64,6 +74,6 @@ it under the same terms as Perl itself.
 
 =head1 SEE ALSO
 
-L<Plack::Middleware::WarnStash> L<Plack::Middleware::Test::StashWarnings>
+L<Plack::Middleware::Test::StashWarnings>
 
 =cut
